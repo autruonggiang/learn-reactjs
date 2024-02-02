a99e26712fca82099d63e5f088a41d6b6fdf0a5d
autruonggiang <21522019@gm.uit.edu.vn>
2024-02-02 21:13:41 +0700

    feat: learn render elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Giang';
+  const age = 20;
+  const isMale = true;
+  const student = {
+    name: 'ReactJS'
+  };
+  const colorList = ['red', 'green', 'blue'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          autruonggiang
+          autruonggiang uit
         </p>
+
+        <p>Xin chao {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
+        {isMale ? <p>Male</p> : <p>Female</p>}
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+        {isMale && (
+          <div>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </div>
+        )}
+
+        {isMale && (
+          <React.Fragment>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </React.Fragment>
+        )}
+
+        {isMale && (
+          <>
+            <p>Male 1</p>
+            <p>Male 2</p>
+            <p>Male 3</p>
+          </>
+        )}
+
+        <p>{student.name}</p>
+
+        <ul>
+          {colorList.map(color => (
+            <li key={color} style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
-    </div>
+    </div >
   );
 }
 
