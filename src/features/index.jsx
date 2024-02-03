15a711e943fcb1b8551b1f17919bbb425b551c55
autruonggiang <21522019@gm.uit.edu.vn>
2024-02-03 20:51:16 +0700

    feat: folder structure

---
+++ src/features/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+TodoFeature.propTypes = {
+
+};
+
+function TodoFeature(props) {
+    return (
+        <div>
+            TodoFeature
+        </div>
+    );
+}
+
+export default TodoFeature;
