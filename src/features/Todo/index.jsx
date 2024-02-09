24876c9e702c5cf9b517a7914bf085aec9a33617
autruonggiang <21522019@gm.uit.edu.vn>
2024-02-09 20:17:58 +0700

    feat: Filter Todo lab

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -26,6 +26,7 @@ function TodoFeature({ props }) {
     ]
 
     const [todoList, setTodoList] = useState(initTodoList);
+    const [filteredStatus, setFilteredStatus] = useState('all');
 
     const handleTodoClick = (todo, idx) => {
         // clone current array to the new one
@@ -44,10 +45,31 @@ function TodoFeature({ props }) {
         setTodoList(newTodoList);
     }
 
+    const handleShowAllClick = () => {
+        setFilteredStatus('all');
+    }
+
+    const handleShowCompletedClick = () => {
+        setFilteredStatus('completed');
+    }
+
+    const handleShowNewClick = () => {
+        setFilteredStatus('new');
+    }
+
+    const renderedTodoList = todoList.filter(todo => filteredStatus === 'all' || filteredStatus === todo.status);
+    // console.log(renderTodoList);
+
     return (
         <div>
             <h3>Todo List</h3>
-            <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
+            <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
+
+            <div>
+                <button onClick={(handleShowAllClick)}>Show All</button>
+                <button onClick={(handleShowCompletedClick)}>Show Completed</button>
+                <button onClick={(handleShowNewClick)}>Show New</button>
+            </div>
         </div>
     );
 }
